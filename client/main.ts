6b648452e4e9bafa58b58e96b853fb062f44f717
jwoods1 <jwoods1@fullsail.edu>
2016-03-30 23:26:40 -0600

    init commit meteor 1.3 with angular 2

---
+++ client/main.ts
@@ -0,0 +1,10 @@
+ import {Component} from 'angular2/core';
+ import {bootstrap} from 'angular2/bootstrap';
+
+    @Component({
+      selector: 'break',
+       templateUrl: "client/layout/nav.html"
+    })
+    class Break {}
+
+    bootstrap(Break);
